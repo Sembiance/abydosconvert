a0d787e056dfed9cb38f583293af8feeaaf99398
Robert Schultz <robert@cosmicrealms.com>
2020-09-09 10:45:26 -0400

    Now converts all pages instead of just the default page

--- src/abydosconvert.c
+++ src/abydosconvert.c
@@ -22,37 +22,50 @@ void abydosconvert(void)
 		exit(EXIT_FAILURE);
 	}
 
+	int pageCount = abydos_get_page_count(ar);
+
 	abydos_info_t info;
 	abydos_get_info(ar, &info);
 	printf("{ \"width\" : %d", info.width);
 	printf(", \"height\" : %d", info.height);
 	printf(", \"scalable\" : %s", info.features&ABYDOS_FEATURE_SCALABLE_SIZE ? "true" : "false");
 	printf(", \"animated\" : %s", abydos_can_animate(ar) ? "true" : "false");
-	printf(", \"pages\" : %d", abydos_get_page_count(ar));
+	printf(", \"pages\" : %d", pageCount);
 	printf(", \"layers\" : %d", abydos_get_layer_count(ar));
 	printf(", \"variants\" : %d", abydos_get_variant_count(ar));
 	printf(", \"frames\" : %d", abydos_get_frame_count(ar));
 	printf(", \"scalableTime\" : %s }\n", info.features&ABYDOS_FEATURE_SCALABLE_TIME ? "true" : "false");
 
-	if(info.features&ABYDOS_FEATURE_SCALABLE_SIZE || abydos_can_animate(ar))
+	for(int i=0;i<pageCount;i++)
 	{
-		if(info.features&ABYDOS_FEATURE_SCALABLE_SIZE)
-		{
-			sprintf(outputFilePath, "%s/%s.svg", gConfig.outputDirPath, basename);
-			abydos_write_to_svg(ar, outputFilePath);
-		}
+		char suffix[16];
+		if(pageCount==1)
+			memset(suffix, 0, 16);
+		else
+			sprintf(suffix, ".%03d", i);
+		
+		abydos_set_page(ar, i);
 
-		if(abydos_can_animate(ar))
+		if(info.features&ABYDOS_FEATURE_SCALABLE_SIZE || abydos_can_animate(ar))
 		{
-			sprintf(outputFilePath, "%s/%s.webp", gConfig.outputDirPath, basename);
-			abydos_write_to_webp(ar, outputFilePath);
+			if(info.features&ABYDOS_FEATURE_SCALABLE_SIZE)
+			{
+				sprintf(outputFilePath, "%s/%s%s.svg", gConfig.outputDirPath, basename, suffix);
+				abydos_write_to_svg(ar, outputFilePath);
+			}
+
+			if(abydos_can_animate(ar))
+			{
+				sprintf(outputFilePath, "%s/%s%s.webp", gConfig.outputDirPath, basename, suffix);
+				abydos_write_to_webp(ar, outputFilePath);
+			}
+		}
+		else
+		{
+			sprintf(outputFilePath, "%s/%s%s.png", gConfig.outputDirPath, basename, suffix);
+			abydos_write_to_png(ar, outputFilePath);
 		}
 	}
-	else
-	{
-		sprintf(outputFilePath, "%s/%s.png", gConfig.outputDirPath, basename);
-		abydos_write_to_png(ar, outputFilePath);
-	}
-	
+
     return;
 }
